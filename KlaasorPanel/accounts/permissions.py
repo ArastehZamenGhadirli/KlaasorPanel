5e87b8206cb93f0473a53bc97b294345cd58c512
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-04-29 00:37:38 +0330

    permissio.py_account_

--- KlaasorPanel/accounts/permissions.py
+++ KlaasorPanel/accounts/permissions.py
@@ -1,5 +1,38 @@
 from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
+
+
+class IsMentor(permissions,BasePermission):
+    def has_permission(self,request,view):
+        return request.user.groups.filter(name='MENTOR').exists()
+
+
+class IsFinacialTeam(permissions,BasePermission):
+    def has_permission(self,request,view):
+        return request.user.groups.filter(name='FINANCIAL').exists()
+
+
+
+
+
+class IsTeacherOrMentor(permissions,BasePermission):
+    def has_permission(self,request,view):
+        return request.user.groups.filter(name__in=['MENTOR','TEACHER']).exists()
+
+
+
+class IsRegisterSupport(permissions,BasePermission):
+    def has_permission(self,request,view):
+        return request.user.groups.filter(name='REGISTER').exists()
+
+class IsTicketSupport(permissions,BasePermission):
+    def has_permission(self,request,view):
+        return request.user.groups.filter(name='TICKET').exists()
+
+
+    
+    
 
 
 
